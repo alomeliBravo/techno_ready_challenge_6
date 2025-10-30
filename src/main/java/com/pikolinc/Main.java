ac7a27a5af8a9d69a368e63f176c22bffd150420
Angel Lomeli <alomelibravo@gmail.com>
2025-10-30 11:52:45 -0600

    feature: add ItemApiRoutes to the list of routes

--- src/main/java/com/pikolinc/Main.java
+++ src/main/java/com/pikolinc/Main.java
@@ -3,11 +3,15 @@ package com.pikolinc;
 
 import com.pikolinc.config.database.JdbiProvider;
 import com.pikolinc.config.json.GsonProvider;
+import com.pikolinc.controller.ItemController;
 import com.pikolinc.controller.UserController;
 import com.pikolinc.exception.GlobalExceptionHandler;
+import com.pikolinc.repository.impl.JdbiItemRepository;
 import com.pikolinc.repository.impl.JdbiUserRepository;
+import com.pikolinc.routes.ItemApiRoutes;
 import com.pikolinc.routes.Router;
 import com.pikolinc.routes.UserApiRoutes;
+import com.pikolinc.service.impl.ItemServiceImpl;
 import com.pikolinc.service.impl.UserServiceImpl;
 import spark.Spark;
 
@@ -19,7 +23,8 @@ public class Main {
         JdbiProvider jdbiProvider = new JdbiProvider();
         jdbiProvider.connect();
         List<Router> routes = List.of(
-                new UserApiRoutes(new UserController(new UserServiceImpl(new JdbiUserRepository(jdbiProvider)), new GsonProvider()))
+                new UserApiRoutes(new UserController(new UserServiceImpl(new JdbiUserRepository(jdbiProvider)), new GsonProvider())),
+                new ItemApiRoutes(new ItemController(new ItemServiceImpl(new JdbiItemRepository(jdbiProvider)), new GsonProvider()))
         );
         routes.forEach(Router::initRoute);
         GlobalExceptionHandler.init();
