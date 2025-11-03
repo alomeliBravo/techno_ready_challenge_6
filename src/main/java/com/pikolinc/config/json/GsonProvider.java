654c6fccd49158e76f34a80210711d244fa7b8df
Angel Lomeli <alomelibravo@gmail.com>
2025-11-03 00:21:51 -0700

    feature(JsonProvider): add no args constructor of GsonProvider to deserialize correctly the offerStatus field

--- src/main/java/com/pikolinc/config/json/GsonProvider.java
+++ src/main/java/com/pikolinc/config/json/GsonProvider.java
@@ -1,10 +1,28 @@
 package com.pikolinc.config.json;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonParseException;
 import com.pikolinc.config.JsonProvider;
+import com.pikolinc.enums.OfferStatus;
 
 public class GsonProvider implements JsonProvider {
-    private final Gson gson = new Gson();
+    private final Gson gson;
+
+    public GsonProvider() {
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(OfferStatus.class, (JsonDeserializer<OfferStatus>) (json, typeOfT, context) -> {
+                    String value = json.getAsString();
+                    if (value == null) return null;
+                    try {
+                        return OfferStatus.valueOf(value.toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        throw new JsonParseException("Invalid OfferStatus: " + value + " OfferStatus accepted: ACCEPTED, PENDING, REJECTED");
+                    }
+                })
+                .create();
+    }
 
     @Override
     public <T> T fromJson(String json, Class<T> type) {
