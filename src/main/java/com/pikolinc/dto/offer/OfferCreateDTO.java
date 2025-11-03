1d4f8d03a61305ab13b54b66c3230161d218f168
Angel Lomeli <alomelibravo@gmail.com>
2025-11-03 00:23:09 -0700

    refactor(dto): add extra validations to not accept null values on offerStatus and amount properties (OfferCreateDTO)

--- src/main/java/com/pikolinc/dto/offer/OfferCreateDTO.java
+++ src/main/java/com/pikolinc/dto/offer/OfferCreateDTO.java
@@ -2,7 +2,6 @@ package com.pikolinc.dto.offer;
 
 import com.pikolinc.enums.OfferStatus;
 import com.pikolinc.utils.ValidOfferStatus;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -13,8 +12,10 @@ public record OfferCreateDTO(
         @NotNull(message = "itemId is required")
         @Positive(message = "itemId must be greater than zero")
         Long itemId,
+        @NotNull(message = "offerStatus is required")
         @ValidOfferStatus()
         OfferStatus offerStatus,
+        @NotNull(message = "amount is required")
         @Positive(message = "Price must be greater than zero")
         Double amount
 ) {}
